9de4dca6c311588de01d0c5a57e321c24386a4de
cardo1011 <ricardomunoz4250@gmail.com>
2023-01-03 00:00:02 -0800

    navbar

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// Declare variables
-var recipeUrl = "www.themealdb.com/api/json/v1/1/filter.php?c=" + category "&parameter";
-var category = 'dropdown list selection';
+// // Declare variables
+// var recipeUrl = "www.themealdb.com/api/json/v1/1/filter.php?c=" + category "&parameter";
+// var category = 'dropdown list selection';
 
 
-// if an option from dropdown list is selected and the button is clicked, showRecipe();
+// // if an option from dropdown list is selected and the button is clicked, showRecipe();
 
 
 
 
-var showRecipe = function (category) {
+// var showRecipe = function (category) {
     
-    fetch(recipeUrl)
-        .then(function(response) {
-            if (response.ok) {
-                console.log(response);
-            } else {
-                alert('Error' + response.statusText);
-            }
-        })
-        .catch(function(error) {
-            alert('Unable to connect to MealDB');
-        })
-}
+//     fetch(recipeUrl)
+//         .then(function(response) {
+//             if (response.ok) {
+//                 console.log(response);
+//             } else {
+//                 alert('Error' + response.statusText);
+//             }
+//         })
+//         .catch(function(error) {
+//             alert('Unable to connect to MealDB');
+//         })
+// }
